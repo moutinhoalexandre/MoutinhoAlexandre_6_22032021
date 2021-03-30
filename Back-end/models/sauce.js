0d2fc66739b09d7583b205003a2f64223cb50aae
Moutinho <alexandre.moutinho@live.fr>
2021-03-30 17:56:42 +0200

    :wrench: creation of a data schema

---
+++ Back-end/models/sauce.js
@@ -0,0 +1,19 @@
+//On crée un schéma de données grâce à mongoose avec les propriétés désirées
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageURL: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: String, required: true },
+  usersDisliked: { type: String, required: true },
+});
+
+//Puis on l'exporte en tant que modèle
+module.exports = mongoose.model("Sauce", sauceSchema);
